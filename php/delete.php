fd173052f03fde6a17529bb83a0772d2914b6243
Jim <42741275+JimmyIb@users.noreply.github.com>
2018-12-05 18:47:48 -0500

    organized the forms

---
+++ php/delete.php
@@ -0,0 +1,16 @@
+<?php
+	$servername = 'localhost';
+	$username = 'root';
+	$password = '';
+	$dbname = 'yokotest';
+
+	$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+	$sql = "DELETE FROM client WHERE clientid='$_GET[id]'";
+
+	if(mysqli_query($conn, $sql))
+		header("refresh:1; url=../databaseSite.php");
+	else
+		echo "Not deleted";
+
+?>
